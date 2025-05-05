95974f15953e12892b73ba1102473ed1fe1303a4
ASUR <153576791+AsurLeecher@users.noreply.github.com>
2025-05-05 23:21:05 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,5 +1,5 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", "3673617983"))
-API_HASH = environ.get("API_HASH", "fe21db9f61ff01300fc1fa3673617983")
-BOT_TOKEN = environ.get("BOT_TOKEN", "7660015741:AAFQakWCiJjxXXXEbtbLDEOExa6IymCK1rs")
+API_ID = int(environ.get("API_ID", "25567551"))
+API_HASH = environ.get("API_HASH", "45993062e6b160a7a360d648ecc8e43a")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7537368666:AAGIveG6g9ilEv_IHXr5KCRidXIxcqwLMEA")
